a60e991864ce684907478384c7b67b7b444adef3
Paul Scala <89755484+pscala22@users.noreply.github.com>
2022-11-30 22:51:46 -0500

    Additional calls

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ df = pd.read_csv('online_retail.csv')
 print('File is Observed')
 print (df)
 
-#Removing rows whose StockCode or Invoice values contain non-digit characters
-print ('Removing rows whose StockCode or Invoice values contain non-digit characters')
-
-# Checking values
-numeric = df[df.StockCode.apply(lambda x: x.isnumeric())& df.Invoice.apply(lambda x: x.isnumeric())]
-
-# Display result
-print("Result:\n",numeric)
+def changes():
+    #Removing rows whose StockCode or Invoice values contain non-digit characters
+    print ('Removing rows whose StockCode or Invoice values contain non-digit characters')
+    print (df[df.StockCode.apply(lambda x: x.isnumeric())& df.Invoice.apply(lambda x: x.isnumeric())])
+    #removing rows whose Price values are less than 10
+    print ('removing rows whose Price values are less than 10')
+    print (df[df['Price'] > 10]) 
+    #removing rows whose country values are not equal to "United Kingdom", "Italy", "France", "Germany", "Norway", "Finland", "Austria", "Belgium", "European Community", "Cyprus", "Greece", "Iceland", "Malta", "Netherlands", "Portugal", "Spain", "Sweden", or "Switzerland"
+    print ('removing rows whose country values are not equal to "United Kingdom", "Italy", "France", "Germany", "Norway", "Finland", "Austria", "Belgium", "European Community", "Cyprus", "Greece", "Iceland", "Malta", "Netherlands", "Portugal", "Spain", "Sweden", or "Switzerland"')
+    countries = ["United Kingdom", "Italy", "France", "Germany", "Norway", "Finland", "Austria", "Belgium", "European Community", "Cyprus", "Greece", "Iceland", "Malta", "Netherlands", "Portugal", "Spain", "Sweden", "Switzerland"]
+    print (df[df.Country.isin(countries)])
+    
+changes()
